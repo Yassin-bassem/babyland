ff96dcb502e2c30324d2a6a361ae9c0392220112
mini-me99 <mini-me99@github.com>
2026-06-23 02:41:29 +0300

    build: generate .env file dynamically during build process

---
+++ generate-env.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const envKeys = [
+  'VITE_SUPABASE_URL',
+  'VITE_SUPABASE_PUBLISHABLE_KEY',
+  'VITE_GEMINI_API_KEY',
+  'VITE_TELEGRAM_BOT_TOKEN',
+  'VITE_TELEGRAM_CHAT_ID'
+];
+
+let envContent = '';
+envKeys.forEach(key => {
+  const val = process.env[key];
+  if (val) {
+    envContent += `${key}="${val}"\n`;
+  }
+});
+
+fs.writeFileSync('.env', envContent);
+console.log('.env file generated successfully for build!');
